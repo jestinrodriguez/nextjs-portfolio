277660ae080ecf0ca0e3e9d3aa37728531014e31
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-03-15 16:34:07 -0700

    test

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -11,23 +11,23 @@ const Testimonials = () => {
     <div className='h-full bg-primary/30 py-32 text-center'>
       <div className='container mx-auto h-full flex flex-col justify-center'>
         {/* title */}
-        <motion.h2
+        {/* <motion.h2
           variants={fadeIn('up', 0.2)}
           initial='hidden'
           animate='show'
           exit='hidden'
           className='h2 mb-8 xl:mb-0'>
           What clients <span className='text-accent'>say.</span>
-        </motion.h2>
+        </motion.h2> */}
         {/* slider */}
-        <motion.div
+        {/* <motion.div
           variants={fadeIn('up', 0.4)}
           initial='hidden'
           animate='show'
           exit='hidden'  
-        >
+        > */}
           {/* <TestimonialSlider/> */}
-        </motion.div>
+        {/* </motion.div> */}
       </div>
     </div>
   );
