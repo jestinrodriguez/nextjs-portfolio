59ce77676a5068742d08ba786c2b6607fd0c63bc
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2024-03-14 20:34:28 -0700

    fix tailwind bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,10 +27,10 @@ module.exports = {
         accent: '#F13024',
       },
       backgroundImage: {
-        explosion: 'url("./bg-explosion.png")',
-        circles: 'url("./bg-circles.png")',
-        circleStar: 'url("./circle-star.svg")',
-        site: 'url("./site-bg.svg")',
+        explosion: 'url("/nextjs-portfolio/bg-explosion.png")',
+        circles: 'url("/nextjs-portfolio/bg-circles.png")',
+        circleStar: 'url("/nextjs-portfolio/circle-star.svg")',
+        site: 'url("/nextjs-portfolio/site-bg.svg")',
       },
       animation: {
         'spin-slow': 'spin 6s linear infinite',
